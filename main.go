f61df0d2aa00f59d35cf56f141734696ae2943b9
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2024-11-29 14:26:29 +0100

    created a more intuitive separation of riskreward ratio into separate sliders.

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ func main() {
 
 // This function is exported to JavaScript, so can be called using
 // exports.kc() in JavaScript.
-//export kc 
-func kc(x, y int) int {
-    return x - (100-x)/y
+//
+//export kc
+func kc(p int, re int, ri int) int {
+	return p - (100*ri-p*ri)/re
 }
